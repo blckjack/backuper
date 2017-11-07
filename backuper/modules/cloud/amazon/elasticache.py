9e082296e57b3ef0c37f9f9d65d03a2a5d81eba8
blckjack <1@0.rubley>
2017-11-07 16:58:11 +0200

    add engine

--- backuper/modules/cloud/amazon/elasticache.py
+++ backuper/modules/cloud/amazon/elasticache.py
@@ -45,6 +45,7 @@ class Main(object):
 
     def create_snapshot(self):
         response = self.client.create_snapshot(
+            Engine=self.parameters['engine'],
             SnapshotName=self.parameters['snapshotId'],
             CacheClusterId=self.parameters['databaseId']
         )
@@ -52,6 +53,7 @@ class Main(object):
 
     def restore_from_snapshot(self):
         response = self.client.create_cache_cluster(
+            Engine=self.parameters['engine'],
             SnapshotName=self.parameters['snapshotId'],
             CacheClusterId=self.parameters['databaseId']
         )
@@ -59,18 +61,21 @@ class Main(object):
 
     def delete_snapshot(self):
         response = self.client.delete_snapshot(
+            Engine=self.parameters['engine'],
             SnapshotName=self.parameters['snapshotId']
         )
         return response
 
     def snapshot_is_available(self):
         response = self.client.describe_snapshots(
+            Engine=self.parameters['engine'],
             SnapshotName=self.parameters['snapshotId'])
         response_status = response['Snapshots'][0]['SnapshotStatus']
         return response_status
 
     def cache_cluster_is_available(self):
         response = self.client.describe_cache_clusters(
+            Engine=self.parameters['engine'],
             CacheClusterId=self.parameters['databaseId'])
         response_status = response['CacheClusters'][0]['CacheClusterStatus']
         return response_status
